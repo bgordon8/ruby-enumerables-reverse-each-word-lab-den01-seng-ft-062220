c980e83f6aefd1e9b21287fc0990e7a041bd5758
scruffy-node-9597 <gordonbrian16@gmail.com>
2020-06-12 17:56:51 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,20 @@
+def reverse_each_word(string)
+#new_reversed_string = " "
+  # reversed_string = string.split(" ")
+  # reversed_string.each do |element|
+  # new_reversed_string = element.reverse
+  #   sentence = new_reversed_string.join
+  #   p sentence
+  
+  # end
+  new_element = " "
+  new_arr = string.split(" ")
+  new_arr.each do |element|
+   new_element = element.reverse
+  p new_element
+   end
+    
+
+  
+
+end
