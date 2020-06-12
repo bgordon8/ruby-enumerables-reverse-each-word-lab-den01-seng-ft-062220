5df4eb417b29ff7e71cf668ceb4586badd15bc8a
scruffy-node-9597 <gordonbrian16@gmail.com>
2020-06-12 18:45:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,20 +1,11 @@
 def reverse_each_word(string)
-#new_reversed_string = " "
-  # reversed_string = string.split(" ")
-  # reversed_string.each do |element|
-  # new_reversed_string = element.reverse
-  #   sentence = new_reversed_string.join
-  #   p sentence
-  
-   #end
-   
-   new_element = []
+ new_element = []
    new_arr = string.split(" ")
    new_arr.collect do |element|
      new_element << element.reverse
    
-   end
   p new_element.join(" ")
+   end
   
 
   
